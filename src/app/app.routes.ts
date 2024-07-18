e8e8dfac0fedfc6f844e616629d69a5ae86da8b4
IcaroCarvalho <icaropc16@gmail.com>
2024-07-17 23:47:42 -0300

    Inicio da codificação da tela de login e trocar senha

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './paginas/login/login.component';
+import { TrocarSenhaComponent } from './paginas/trocar-senha/trocar-senha.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    {path: 'trocar-senha', component: TrocarSenhaComponent}
+];
