ee8f6fd41a392456c3b701278d67eb1dd4bcf907
Icaro Carvalho <icaropc16@gmail.com>
2024-07-25 10:15:31 -0300

    Adição dos botões de status da tela de solicitação de ajuste

--- src/app/paginas/solicitacoes-ajustes/solicitacoes-ajustes.component.ts
+++ src/app/paginas/solicitacoes-ajustes/solicitacoes-ajustes.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-solicitacoes-ajustes',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './solicitacoes-ajustes.component.html',
   styleUrl: './solicitacoes-ajustes.component.css'
 })
 export class SolicitacoesAjustesComponent {
+  faSearch = faSearch;
+
+  selectedStatus = 'Pendentes';
+  indicatorPosition = '0%';
+
+  selectStatus(status: string) {
+    this.selectedStatus = status;
+    switch (status) {
+      case 'Pendentes':
+        this.indicatorPosition = '0%';
+        break;
+      case 'Aprovados':
+        this.indicatorPosition = '33.33%';
+        break;
+      case 'Reprovados':
+        this.indicatorPosition = '66.66%';
+        break;
+    }
+  }
 
 }
